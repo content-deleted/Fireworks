b4f440f357a9fbe200c85487869e20064f18250b
content-deleted <contentdeleted@gmail.com>
2019-02-05 15:09:14 -0700

    feat: added new control scripts tweeked player values

---
+++ Assets/Scripts/PlayerScripts/PlayerState.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/* 
+ * This is a singleton which stores stateful information on the player not contained in other classes
+ */
+public class PlayerState : MonoBehaviour
+{
+    public static GameObject Player;
+    public static PlayerState singleton;
+
+    public bool pointerMode = false;
+    public GameObject PlayerControlModel;
+    public GameObject PointerModel;
+
+    public void Awake () {
+        Player = gameObject;
+        singleton = this;
+    } 
+
+    public void Update () {
+        if(OVRInput.GetDown(OVRInput.Button.Back)) {
+            pointerMode = !pointerMode;
+
+            // Set the model to a different one
+            PlayerControlModel.SetActive(!pointerMode);
+            PointerModel.SetActive(pointerMode);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/PointerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerController : MonoBehaviour
+{
+    const int layerNum = 1 << 9; 
+    void Update()
+    {
+        if( PlayerState.singleton.pointerMode && OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger) ){ 
+            RaycastHit hit;
+            // Test
+            if (Physics.Raycast(transform.position, transform.forward, out hit, Mathf.Infinity, layerNum))
+            {
+                Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.yellow);
+            }
+        }
+    }
+}
