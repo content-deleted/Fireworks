10c0beb3d7f33c5c1ee0df038e165a6a1af799d9
dlauer1 <you@example.com>
2019-02-14 22:56:44 -0700

    added PickUp start

--- Assets/Scripts/ItemScripts/ShowUI.cs
+++ Assets/Scripts/ItemScripts/ShowUI.cs
@@ -7,13 +7,20 @@ public class ShowUI : MonoBehaviour
 {
     [SerializeField] private GameObject customSprite;
     private GameObject objClone;
+
+    Animator animator;
+    
+    void Start() => animator = PlayerState.singleton.GetComponent<Animator>();
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             objClone = Instantiate(customSprite, other.transform.position + Vector3.up * 2, Quaternion.identity);
+            animator.SetTrigger("PickUp");
         }
+        
     }
+    
     void OnTriggerExit(Collider other)
     {
         Destroy(objClone);
