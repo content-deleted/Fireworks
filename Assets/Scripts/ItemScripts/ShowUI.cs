b8b4020a248217e3f8bd21c1546b3a4ed59c6d87
dlauer1 <you@example.com>
2019-02-15 00:19:04 -0700

    PickDown added

--- Assets/Scripts/ItemScripts/ShowUI.cs
+++ Assets/Scripts/ItemScripts/ShowUI.cs
@@ -9,7 +9,11 @@ public class ShowUI : MonoBehaviour
     private GameObject objClone;
 
     Animator animator;
-    
+
+    private bool skip = false;
+
+    private int FRAMESTOWAIT = 30;
+
     void Start() => animator = PlayerState.singleton.GetComponent<Animator>();
     void OnTriggerEnter(Collider other)
     {
@@ -17,13 +21,31 @@ public class ShowUI : MonoBehaviour
         {
             objClone = Instantiate(customSprite, other.transform.position + Vector3.up * 2, Quaternion.identity);
             animator.SetTrigger("PickUp");
+            UIDisplay.singleton.Active = true;
+            StartCoroutine(endPickup());
         }
-        
     }
-    
+
+    IEnumerator endPickup()
+    {
+        if(skip)
+        for (int i = 0; i <= FRAMESTOWAIT; i++)
+        {
+            yield return new WaitForEndOfFrame();
+            animator.SetTrigger("PickDown");
+           break;
+        }
+
+        UIDisplay.singleton.Active = false;
+        Destroy(objClone);
+    }
+
+    void Update () {
+        if(OVRInput.GetDown(OVRInput.Button.Any) || Input.GetKeyDown(KeyCode.Space) ) skip = true;
+    }
+
     void OnTriggerExit(Collider other)
     {
-        Destroy(objClone);
         Destroy(gameObject);
     }
 }
