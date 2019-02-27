9c55e3c3c78a360aebafde57fd43426506ba1185
dlauer1 <you@example.com>
2019-02-26 19:17:22 -0700

    elevator script

---
+++ Assets/Scripts/EnvironmentScripts/Elevator.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    public GameObject moveElevator;
+    public bool moveDown = false;
+    public float y;
+
+    void Start()
+    {
+        y = moveElevator.transform.position.y;
+    }
+    private void OnTriggerStay()
+    {
+        moveElevator.transform.position += Vector3.up * Time.deltaTime;
+    }
+
+    void Update()
+    {
+        if (moveDown)
+        {
+            moveElevator.transform.position += Vector3.down * Time.deltaTime;
+            if (moveElevator.transform.position.y <= y)
+            {
+                moveDown = false;
+            }
+        }
+
+    }
+
+    private void OnTriggerExit()
+    {
+        moveDown = true;
+        moveElevator.transform.position += Vector3.down * Time.deltaTime;
+    }
+}
