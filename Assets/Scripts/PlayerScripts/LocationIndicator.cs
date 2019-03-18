cfea2987c178cb6deb21332a18b42daa5aac5a98
content-deleted <contentdeleted@gmail.com>
2019-03-18 13:58:06 -0700

    fix: improved arrow rotation

--- Assets/Scripts/PlayerScripts/LocationIndicator.cs
+++ Assets/Scripts/PlayerScripts/LocationIndicator.cs
@@ -14,19 +14,20 @@ public class LocationIndicator : MonoBehaviour
         rend = GetComponent<Renderer>();
         rend.material.SetFloat("_Alpha", 0);
     }
+    public Vector3 rot;
     private float ticker;
     public float offsetSpeed;
     void LateUpdate()
     {
-        ticker+=offsetSpeed;
+        ticker += offsetSpeed;
         if(!playerRend.isVisible) {
             showArrow = true;
             
             //put the arrow rotate around
             var dirToPlayer = transform.parent.InverseTransformDirection((playerRend.transform.position - transform.parent.position).normalized)/(10+(Mathf.Cos(ticker)+1) );
             transform.localPosition = new Vector3(dirToPlayer.x,dirToPlayer.y,0.15f);
-            transform.LookAt(transform.parent.TransformDirection(-dirToPlayer*2) );
-            transform.Rotate(180,0,0);
+            transform.LookAt(transform.parent.TransformDirection(-dirToPlayer*2),transform.parent.forward );
+            transform.Rotate(rot,Space.Self);
         }
         else showArrow = false;
 
@@ -36,6 +37,8 @@ public class LocationIndicator : MonoBehaviour
         else if(a > 0) rend.material.SetFloat("_Alpha", a-fadeSpeed);
     }
 
+
+    // Consider we may want to show if the char is on screen that they are behind an object
     bool onScreen(Transform t) {
         var screenPos = Camera.main.WorldToViewportPoint(t.position);
         return screenPos.x > 0 && screenPos.x < 1 && screenPos.y > 0 && screenPos.y < 1;
