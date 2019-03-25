74a32bc7e605c137cc55ac48ce090eaff6fc268b
dlauer1 <you@example.com>
2019-03-24 23:19:07 -0700

    new branch

--- Assets/Scripts/EnvironmentScripts/HelpTexts.cs
+++ Assets/Scripts/EnvironmentScripts/HelpTexts.cs
@@ -9,8 +9,19 @@ public class HelpTexts : MonoBehaviour
     [SerializeField]
     public List<string> helpMessages = new List<string>();
 
-
     public bool triggerOnce;
+
+    void Start()
+    {
+        helpMessages.Add("this is a test");
+        helpMessages.Add("this is a test2");
+        helpMessages.Add("this is a test3");
+
+        for (int i = helpMessages.Count - 1; i >= 0; i--)
+        {
+            helpMessages.RemoveAt(i);
+        }
+    }
 
     void OnTriggerEnter(Collider other)
     {
