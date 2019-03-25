8856f5c7277b6eca285c5275dc49af86d8b2adb8
content-deleted <contentdeleted@gmail.com>
2019-03-24 21:28:23 -0700

    feat: element combination feedback

--- Assets/ElementCombine.cs
+++ Assets/ElementCombine.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using static GameStateManager;
 using System.Linq;
+using UnityEngine.UI;
 public class ElementCombine : MonoBehaviour
 {
     public static List<elements> combined = new List<elements>();
+    public static float displayTime = 5;
     public static void addElement(elements e)
     {
         combined.Add(e);
@@ -28,16 +30,42 @@ public class ElementCombine : MonoBehaviour
             }
         }
         if(!valid){
-            //clear combined
+            // clear combined
             combined = new List<elements>();
+            // show feedback
+            var objClone = Instantiate(Resources.Load("UI_Display") as GameObject, t.position + Vector3.up * 4, Quaternion.identity);
+            objClone.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("X");
+            Destroy(objClone, displayTime);
         }
+
+        // update feedback 
+        t.GetChild(0).GetComponent<Text>().text = (combined.Any()) ? toString() : "";
+
     }
     
     public static void craftChemical(chemical chem) {
         chem.crafted = true;
-        var objClone = Instantiate(Resources.Load($"UI_Display") as GameObject, t.position + Vector3.up * 4, Quaternion.identity);
+        var objClone = Instantiate(Resources.Load("UI_Display") as GameObject, t.position + Vector3.up * 4, Quaternion.identity);
         objClone.GetComponent<SpriteRenderer>().sprite = chem.chemSprite;
-        Destroy(objClone, 3);
+        Destroy(objClone, displayTime);
+
+        // clear combined
+        combined = new List<elements>();
+    }
+
+    public static string toString(){
+        var sorted = combined.OrderBy(x => (int)x);
+        string r = sorted.First().ToString();
+        int inc = 0;
+        for(int i = 1; i < sorted.Count(); i++){
+            if(sorted.ElementAt(i-1) == sorted.ElementAt(i)) inc++;
+            else {
+                r += getElementName(sorted.ElementAt(i));
+                if(inc>0) r += (inc+1).ToString();
+            }
+        }
+        if(inc>0) r += (inc+1).ToString();
+        return r;
     }
     
     private static Transform t;
