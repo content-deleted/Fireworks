57fb4114c1e39af70332bfaf04365fd7bf006531
dlauer1 <you@example.com>
2019-03-26 17:52:07 -0700

    start msgTimer

--- Assets/Scripts/EnvironmentScripts/SpawnMsgOnEnter.cs
+++ Assets/Scripts/EnvironmentScripts/SpawnMsgOnEnter.cs
@@ -4,34 +4,23 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class HelpTexts : MonoBehaviour
+public class SpawnMsgOnEnter : MonoBehaviour
 {
     [SerializeField]
     public List<string> helpMessages = new List<string>();
 
     public bool triggerOnce;
 
-    void Start()
-    {
-        helpMessages.Add("this is a test");
-        helpMessages.Add("this is a test2");
-        helpMessages.Add("this is a test3");
-
-        for (int i = helpMessages.Count - 1; i >= 0; i--)
-        {
-            helpMessages.RemoveAt(i);
-        }
-    }
-
     void OnTriggerEnter(Collider other)
     {
-        
+
         if (other.CompareTag("Player"))
         {
             PlayerTextUI.singleton.helpMessages.AddRange(helpMessages);
             PlayerTextUI.singleton.startPush();
 
-            if (triggerOnce){
+            if (triggerOnce)
+            {
                 Destroy(this);
             }
         }
