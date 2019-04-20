d795883df95a57b06183804fc718d7d4b859ab88
content-deleted <contentdeleted@gmail.com>
2019-04-19 21:41:22 -0700

    fix: minor fixes to nodes

--- Assets/Scripts/PlayerScripts/NodeBasedCamera.cs
+++ Assets/Scripts/PlayerScripts/NodeBasedCamera.cs
@@ -13,7 +13,7 @@ public class NodeBasedCamera : MonoBehaviour
     public void LateUpdate() {
         if(currentNode != null) {
             if(Vector3.Distance(currentNode.position, transform.position) > 0.05f)
-                rb.velocity =  (currentNode.position - transform.position).normalized + (currentNode.position - transform.position) / 8;
+                rb.velocity =  (currentNode.position - transform.position).normalized + (currentNode.position - transform.position) / 10;
             else
                 rb.velocity = Vector3.zero;
         }
